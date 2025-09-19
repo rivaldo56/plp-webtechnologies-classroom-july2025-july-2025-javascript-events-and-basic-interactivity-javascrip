47d25bf658de08a8d379af09fe502ab0f152e32a
rivaldo56 <rivaldofalcao876@gmail.com>
2025-09-19 16:57:46 +0300

    initial commit

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const box = document.getElementById('box-two');
+
+function startAnimation() {
+    box.classList.add('animate');
+}
+
+function stopAnimation() {
+    box.classList.remove('animate');
+}
